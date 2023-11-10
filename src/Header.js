02c1fc54dac3c5ea001a38c9bda8d2e93ee5721e
preston176 <prestonnyamweya@gmail.com>
2023-11-10 16:35:22 +0300

    Add chat and notifications icons to header

--- src/Header.js
+++ src/Header.js
@@ -5,6 +5,8 @@ import HeaderOption from './HeaderOption';
 import HomeIcon from '@mui/icons-material/Home';
 import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from "@mui/icons-material/ChatOutlined"
+import { Notifications } from '@mui/icons-material';
 
 function Header() {
     return (
@@ -25,7 +27,9 @@ function Header() {
                 <HeaderOption Icon={HomeIcon} title="Home" />
                 <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
                 <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
-            
+                <HeaderOption Icon={ChatIcon} title="Messaging" />
+                <HeaderOption Icon={Notifications} title="Notifications" />
+
             </div>
         </div>
     )
