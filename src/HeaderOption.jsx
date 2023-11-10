87e68216935dba6c9013b4fe95add4962cd26261
preston176 <prestonnyamweya@gmail.com>
2023-11-10 16:02:16 +0300

    Add HeaderOption component and CSS file.

---
+++ src/HeaderOption.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./HeaderOption.css"
+
+function HeaderOption() {
+    return (
+        <div className='headerOption'>
+        
+        
+        </div>
+    )
+}
+
+export default HeaderOption
