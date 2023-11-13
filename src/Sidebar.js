cb54d0eab06a2bc9e9b7bd9a645dad356e3bf9b7
preston176 <prestonnyamweya@gmail.com>
2023-11-13 16:34:33 +0300

    Update styles for App and Sidebar components

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,7 +6,7 @@ function Sidebar() {
     return (
         <div className='sidebar'>
             <div className="sidebar__top">
-                <img src="" alt="user-img" />
+                <img src="https://plus.unsplash.com/premium_photo-1661952525120-a01282212dfc?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="user-img" />
                 <Avatar className='sidebar__avatar' />
                 <h2>Preston Mayieka</h2>
                 <h4>Pressym@gmail.com</h4>
