a8bf8794eb9fca3bc6e337e1ec02f94d3a916b12
preston176 <prestonnyamweya@gmail.com>
2023-11-13 16:10:03 +0300

    Add user profile section to Sidebar component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import "./Sidebar.css"
+import { Avatar } from '@mui/material'
 
 function Sidebar() {
     return (
         <div className='sidebar'>
-
+            <div className="sidebar__top">
+                <img src="" alt="user-img" />
+                <Avatar className='sidebar__avatar' />
+                <h2>Preston Mayieka</h2>
+                <h4>Pressym@gmail.com</h4>
+            </div>
+            <div className="sidebar__stats">
+                <div className="sidebar__stat">
+                    <p>Who viewed you</p>
+                    <p className="sidebar__statNumber">
+                        2,543
+                    </p>
+                </div>
+                <div className="sidebar__stat">
+                    <p>Who viewed you</p>
+                    <p className="sidebar__statNumber">
+                        2,543
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
