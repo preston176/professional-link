f03eae4ea5fabbbf3dd0be265e648a1d4ae93939
preston176 <prestonnyamweya@gmail.com>
2023-11-16 14:58:19 +0300

    Add input option for posting photos in Feed component

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./Feed.css"
 import CreateIcon from '@mui/icons-material/Create';
+import ImageIcon from '@mui/icons-material/Image';
+import InputOption from './InputOption';
 
 function Feed() {
     return (
@@ -8,6 +10,13 @@ function Feed() {
             <div className="feed__inputContainer">
                 <div className="feed__input">
                     <CreateIcon />
+                    <form action="">
+                        <input type="text" />
+                        <button type='submit'>Send</button>
+                    </form>
+                </div>
+                <div className="feed__inputOptions">
+                    <InputOption Icon={ImageIcon} color="#70B5F9" title="Photo" />
                 </div>
             </div>
         </div>
