e84d159410540d6fa736dbcc7471a550edbd8fdd
preston176 <prestonnyamweya@gmail.com>
2023-11-16 13:48:05 +0300

    Add create post button to feed

--- src/Feed.js
+++ src/Feed.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import "./Feed.css"
+import CreateIcon from '@mui/icons-material/Create';
 
 function Feed() {
     return (
         <div className='feed'>
-        <div className="feed__inputContainer">
-            
-        </div>
+            <div className="feed__inputContainer">
+                <div className="feed__input">
+                    <CreateIcon />
+                </div>
+            </div>
         </div>
     )
 }
